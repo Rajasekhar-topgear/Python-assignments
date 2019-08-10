3ded32e79a47672c65c259d5b4dfc94dec6af73f
Rajasekhar-topgear <49210110+Rajasekhar-topgear@users.noreply.github.com>
2019-08-11 00:08:35 +0530

    Update Program_17.py

--- Program_17.py
+++ Program_17.py
@@ -3,5 +3,28 @@ num = int(input("Enter how many numbers in the list: "))
 for n in range(num):
     elem = int(input("Enter number:: "))
     numbers.append(elem)
-print("Maximum element in the list is :: ", max(numbers))
-print("Minimum element in the list is :: ", min(numbers))
+print("Biggest element in the list is :: ", max(numbers))
+print("Smallest element in the list is :: ", min(numbers))
+
+def find_small_num():
+    smallest = numbers[0]
+    for n in range(0,num):
+        if (numbers[n] < smallest):
+            smallest = numbers[n]
+
+    print("Smallest number::", smallest)
+
+
+def find_big_num():
+    biggest = numbers[0]
+    for n in range(0,num):
+        if (numbers[n] > biggest):
+            biggest = numbers[n]
+
+    print("Biggest number::", biggest)
+
+
+find_small_num()
+find_big_num()
+
+
